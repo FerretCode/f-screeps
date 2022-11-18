d9d17fbaa789727a20beebd26b8e25873a8f59fe
FerretCode <674313@salemkeizer.org>
2022-11-18 19:49:00 +0000

    fix creep cleaning

--- clean.js
+++ clean.js
@@ -4,7 +4,7 @@ module.exports = {
       const gameRoom = Game.rooms[room];
 
       for (const role of Object.keys(gameRoom.creeps))
-        for (creep of gameRoom.creeps[role])
+        for (const creep of gameRoom.creeps[role])
           if (!Game.creeps[creep.name]) {
             delete Memory.creeps[creep.name];
             gameRoom.creeps[role].splice(
